705dc85d1e311cee4600c77c18a002939143d8e2
Cosmin Adrian Lungu <adrian@adrianalter.com>
2021-12-21 23:44:38 +0100

    first

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,34 +1,57 @@
-#Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
+def can_see_stage(matrix):
+    rows = len(matrix)
+    seeStage = True
+    columns = len(matrix[1])
+    #print("rows ", rows)
+    #print("columns ", columns)
 
-def checkAllDifferent(lista):
+    for k in range(columns): #k = 0
+        for i in (range(0, rows - 1)): #i = 0
+            for j in range(columns): #j = 0
+                if(j == k):
+                    #print("sono allineato, valore: ", matrix[i][j])
+                    if(matrix[i][j] >= matrix[i+1][j]):
+                        seeStage = False
+                        print(seeStage)
+                        return False
 
-    doubled = False
+    print(seeStage)
+    return seeStage
 
-    for number in range(0, len(lista)):
-        #print("PRIMO FOR Number: ", number, " valore:", lista[number])
-        for var in range(0, len(lista)):
-            #print("--SECONDO FOR: var: ", var, " valore:", lista[var])
-            if ( (lista[number] == lista[var]) and (number != var)):
-                doubled = True
 
-    if(doubled):
-        print("You have a double!")
-    else:
-        print("Your numbers are all different from each other")
+
+
 
 
 
 if __name__ == '__main__':
-    numbers = [0,1,2,3,4,5,6,7,8,9]
-    checkAllDifferent(numbers)
+    rows = 3
+    columns = 4
+    sum = 0
 
+    can_see_stage([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
 
+    can_see_stage([
+        [0, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ])
 
+    can_see_stage([
+        [2, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ])
 
-
-
-
-
+    can_see_stage([
+        [1, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ])
 
 
 
